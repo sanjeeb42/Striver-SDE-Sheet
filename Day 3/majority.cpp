f890bffd6f3dd5b45b21bf2572034c5c08b34784
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-06-05 12:16:29 +0530

    Update majority.cpp

--- Day 3/majority.cpp
+++ Day 3/majority.cpp
@@ -1,1 +1,37 @@
+#include <bits/stdc++.h> 
+int findMajorityElement(int arr[], int n) {
+	// Write your code here.
+    // Count of majority elemnt will be greater than n/2+ x
+    
+    
+    int element=arr[0];
+    int curr_count=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==element){
+            curr_count++;
+        }
+        else curr_count--;
+        if(curr_count<=0)
+        {
+            element=arr[i];
+            curr_count=1;
+         }
+    }
+    
+    int cnt=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==element){
 
+        cnt++;
+        }
+    }
+    if(cnt<=n/2)
+    {
+        element=-1;
+    }
+    
+    
+    return element;
+}
