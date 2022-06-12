6c5ac7e0178185c2e2e25968ed0056bac2a8386d
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-06-12 13:23:57 +0530

    Update Find_Intersection.cpp

--- Day 6/Find_Intersection.cpp
+++ Day 6/Find_Intersection.cpp
@@ -37,3 +37,43 @@ int findIntersection(Node *firstHead, Node *secondHead)
     }  
     return -1;
 }
+
+
+//Naive Hashmap
+
+// Hashing Approach
+// Traverse the first list and store the address/reference to each node in a hash set/map/dictionary.
+// Then check every node in the second list:
+// If the address of the node of the second list appears in the hash set/map/dictionary, then the node is the intersection node
+// If we do not find any address of the second list node in the hash set/map/dictionary then return -1
+
+int findIntersection(Node *firstHead, Node *secondHead)
+{
+    
+    Node *firstHeadTmp = firstHead;
+
+    // Set to store reference of first list.
+    unordered_set<Node *> referenceSet;
+
+    // Traverse the first list and store the reference in the set.
+    while (firstHeadTmp != NULL)
+    {
+        referenceSet.insert(firstHeadTmp);
+        firstHeadTmp = firstHeadTmp->next;
+    }
+
+    Node *secondHeadTmp = secondHead;
+
+    // Traverse second list, check if the reference of current node is present or not.
+    // If it is present then return the data of that node.
+    while (secondHeadTmp != NULL)
+    {
+        if (referenceSet.find(secondHeadTmp) != referenceSet.end())
+        {
+            return secondHeadTmp->data;
+        }
+        secondHeadTmp = secondHeadTmp->next;
+    }
+
+    return -1;
+}
