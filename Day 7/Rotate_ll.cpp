71f41bef532f5f687b47984243ad20cfa4814b95
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-06-14 00:42:40 +0530

    Update Rotate_ll.cpp

--- Day 7/Rotate_ll.cpp
+++ Day 7/Rotate_ll.cpp
@@ -1,3 +1,47 @@
+// Efficinet approach will be to make the linked list circular in order to minimise the traversal - 2 Traversal to one Traversal
+
+Node *rotate(Node *head, int k) {
+    
+    // Base condition.
+    if(head == NULL) {
+        return head;
+    }
+    
+    int len = 1;
+    Node *temp = head;
+        
+    // Calculate length of the linked list.
+    while(temp->next != NULL) {
+        temp = temp->next;
+        len += 1;
+    }
+    
+    k = k % len;
+    
+    // Number of rotations are same as len so no change in LL.
+    if(k == len || k == 0) {
+        return head;
+    }
+
+    // To make a circular linked list.
+    temp->next = head;
+
+    temp = head;
+    
+    for(int i = 0; i < abs(len - k - 1); i++) {
+        temp = temp->next;
+    }
+
+    // Changing pointers.
+    head = temp->next;
+    temp->next = NULL;
+
+    return head;
+}
+
+
+// Naive- 2 Traversal
+
 int count(Node *head)
 {
     Node* front=head;
