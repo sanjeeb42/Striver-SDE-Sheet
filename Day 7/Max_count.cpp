985c111f308c19a021c2aa916aa1f6744ff7c59c
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-06-15 02:35:31 +0530

    Create Max_count.cpp

---
+++ Day 7/Max_count.cpp
@@ -0,0 +1,15 @@
+  int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt=0;
+        int ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1){
+                cnt++;
+            }
+            else cnt=0;
+            
+            ans=max(ans,cnt);
+        }
+        
+        return ans;
+    }
