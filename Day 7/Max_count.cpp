4045a4ad1a762d28016dae42c8496c9a91e8db47
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-06-15 03:13:38 +0530

    Update Max_count.cpp

--- Day 7/Max_count.cpp
+++ Day 7/Max_count.cpp
@@ -1,4 +1,38 @@
-  int findMaxConsecutiveOnes(vector<int>& nums) {
+//Code Studio
+
+int longestSubSeg(vector<int> &arr , int n, int k){
+    // Write your code here.
+    
+    int ans=0,kk=k,left=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==1){
+            
+        }
+        else if(arr[i]==0)
+        {
+            kk--;
+            if(kk<0)
+            {
+                while(left<i && kk<0)
+                {
+                     if(arr[left]==0)
+                     {
+                         kk++;
+                     }
+                    left++;
+                }
+            }
+        }
+        ans=max(ans,i-left+1);
+    }   
+    return ans;
+}
+
+
+//Leetcode
+
+
+int findMaxConsecutiveOnes(vector<int>& nums) {
         int cnt=0;
         int ans=0;
         for(int i=0;i<nums.size();i++)
