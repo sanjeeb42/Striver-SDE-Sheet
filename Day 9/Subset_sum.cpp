8c9370e8b004a4c0d9d1667ba81942c035409f2e
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-06-17 02:02:27 +0530

    Update Subset_sum.cpp

--- Day 9/Subset_sum.cpp
+++ Day 9/Subset_sum.cpp
@@ -1,3 +1,30 @@
+//Iterative -O(2^n * n)time and O(1) space
+
+vector<int> subsetSum(vector<int> &num)
+{
+    // Write your code here.
+    vector<int>ans;
+    int n=num.size();
+    int no=1<<num.size();
+    for(int i=no-1;i>=0;i--){
+        int curr_sum=0;
+        
+        for(int j=0;j<n;j++){
+            if(i&(1<<j)){
+                curr_sum+=num[j];
+            }
+        }
+        ans.push_back(curr_sum);
+    }
+    sort(ans.begin(),ans.end());
+    return ans;
+}
+
+
+
+
+//recursive -O(2^n) time and O(n) space
+
 void solve(vector<int>&ans,vector<int>nums,int index,int curr_sum){
     if(index==nums.size()){
         ans.push_back(curr_sum);
