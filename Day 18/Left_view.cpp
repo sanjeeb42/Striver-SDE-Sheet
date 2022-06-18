dfdcc6ad42f9cb679cb6583422cbd3de0532fcb6
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-06-18 11:35:55 +0530

    Update Left_view.cpp

--- Day 18/Left_view.cpp
+++ Day 18/Left_view.cpp
@@ -19,3 +19,35 @@ vector<int> getLeftView(TreeNode<int> *root)
     findLeftview(root,0,maxlevel,ans);
     return ans;
 }
+
+
+//Aliter using map, Horizontal distance and verticak distance
+
+void inorder(BinaryTreeNode<int> * root,int hd,map<int,pair<int,int>>&mp,int height)
+{
+    
+    if(root==NULL)return;
+    inorder(root->left,hd-1,mp,height+1); 
+    int curr_height=mp[hd].second;
+    if(height>=curr_height){
+        mp[hd]=make_pair(root->data,height);
+    }
+    
+    inorder(root->right,hd+1,mp,height+1); 
+   
+}
+
+vector<int> bottomView(BinaryTreeNode<int> * root){
+
+    // Write your code here.     
+    //Play game of Horizontal distances
+    //Lmao To store Node and then the hd and the height in the node
+    map<int,pair<int,int>>mp;
+    inorder(root,0,mp,0);
+    vector<int>ans;    
+    for(auto it:mp){
+        ans.push_back(it.second.first);
+    }
+    return ans;
+
+}
