e36760b3650fb7e9ca83e3759cf6dd8c21d60350
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-06-26 22:22:31 +0530

    Update Only_single_occurenece.cpp

--- Day 11/Only_single_occurenece.cpp
+++ Day 11/Only_single_occurenece.cpp
@@ -1,5 +1,64 @@
 #include<bits/stdc++.h>
 
+
+
+int uniqueElement(vector<int>nums, int n)
+{
+//     int n=nums.size();
+        int low=nums[0], high=nums[n-1];
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            int tmp_index=upper_bound(nums.begin(),nums.end(),mid)-nums.begin();
+            if(tmp_index%2==0){
+                low=mid+1;
+            }
+            else high=mid-1;
+            
+        }
+        
+        return low;
+}
+
+
+// Binary Search 2
+
+// The idea here is to use binary search and move left and right using the below observation.
+
+// If mid is even, and arr[mid] == arr[mid + 1], then size of subarray [0...mid-1] (left side) is even (since the every element occurs twice), so left side does not contain the single occurrence element. Hence we need to check in [mid + 1, ..., N] subarray (right side of mid). Else we need to search for the required element on the left side.
+// Example:
+//  [1, 1, 2, 2, 3(mid), 3, 5, 7, 7]
+// Here mid = 4.  Here subarray[0…3] doesn’t contain our unique number. So we need to check in the right side of the mid i.e. subarray [5… 8].
+// If mid is odd, and arr[mid] == arr[mid - 1], then the size of subarray [0...mid - 2] (left side) is even (since every element occurs twice), so the left side does not contain the single occurrence element. Hence we need to check in subarray [mid + 1, ..., N] (right side of mid). Else we need to search for the required element on the left side.
+// Example 
+// [1, 1, 2, 2, 3, 3(mid), 5, 7, 7]
+// Here mid = 5.  Here subarray[0…3] doesn’t contain our unique number. So we need to check in the right side of mid i.e. subarray [6… 8].
+
+int uniqueElement(vector<int> arr, int n)
+{
+	int low = 0, high = n - 1, mid;
+
+	// Do binary search
+	while (low < high) {
+
+		// Calculate mid
+		mid = (low + high) / 2;
+
+		// Update low and high
+		if ((mid % 2 == 1 and arr[mid] == arr[mid - 1]) or (mid % 2 == 0 and arr[mid] == arr[mid + 1]))
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid;
+		}
+	}
+
+	return arr[low];
+}
+
+
 // int uniqueElement(vector<int> arr, int n)
 // {
 // 	// Write your code here
@@ -24,22 +83,6 @@
 // }
 
 
-int uniqueElement(vector<int>nums, int n)
-{
-//     int n=nums.size();
-        int low=nums[0], high=nums[n-1];
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            
-            int tmp_index=upper_bound(nums.begin(),nums.end(),mid)-nums.begin();
-            if(tmp_index%2==0){
-                low=mid+1;
-            }
-            else high=mid-1;
-            
-        }
-        
-        return low;
-}
+
 
 
