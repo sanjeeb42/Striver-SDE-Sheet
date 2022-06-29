bc574a1cf890eede1aea4eb3b24d01c679bf0c7a
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-06-29 11:34:00 +0530

    Update 37_Buy_sell.cpp

--- DP Series/37_Buy_sell.cpp
+++ DP Series/37_Buy_sell.cpp
@@ -22,7 +22,24 @@ int solve(vector<vector<int>>dp,vector<int>&prices,int index,int trans){
 int maxProfit(vector<int>& prices, int n)
 {
     // Write your code here.
-    vector<vector<int>>dp(n,vector<int>(4,-1));
-    return solve(dp,prices,0,0);
+    vector<vector<int>>dp(n+1,vector<int>(5,0));
+   // return solve(dp,prices,0,0);
     
+    for(int index=n-1;index>=0;index--)
+    {
+        for(int trans=3;trans>=0;trans--)
+        {
+             if(trans%2==0)
+            {
+              dp[index][trans]=max(-prices[index]+dp[index+1][trans+1],
+                             0+dp[index+1][trans]);
+            }
+            else 
+            {
+               dp[index][trans]=max(prices[index]+dp[index+1][trans+1],
+                             0+dp[index+1][trans]);
+            }
+        }
+    }
+  return dp[0][0];  
 }
