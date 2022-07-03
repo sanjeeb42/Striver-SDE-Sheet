46f6deb171d1ca545615cc1aa3410ce211d1c991
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-07-03 23:59:29 +0530

    Update Integer_to_roman.cpp

--- Day 15/Integer_to_roman.cpp
+++ Day 15/Integer_to_roman.cpp
@@ -2,39 +2,22 @@ class Solution {
 public:
     string intToRoman(int num) {
         
-        unordered_map<string,int> conv{
-        {"M", 1000},
-        {"D", 500},
-        {"C", 100},
-        {"L", 50},
-        {"X", 10},
-        {"V", 5},
-        {"I", 1},
-        {"IV", 4},
-        {"IX", 9},
-        {"XL", 40},
-        {"XC", 90},
-        {"CD", 400},
-        {"CM", 900},
-        {"", 0}};
+
         
         vector<pair<int,string>>mapping;
         mapping.push_back({1000,"M"});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
-        mapping.push_back({});
+        mapping.push_back({900,"CM"});
+        mapping.push_back({500,"D"});
+        mapping.push_back({400,"CD"});
+        mapping.push_back({100,"C"});
+        mapping.push_back({90,"XC"});
+        mapping.push_back({50,"L"});
+        mapping.push_back({40,"XL"});
+        mapping.push_back({10,"X"});
+        mapping.push_back({9,IX});
+        mapping.push_back({5,"V"});
+        mapping.push_back({4,"IV"});
+        mapping.push_back({1,"I"});
         
         
         
