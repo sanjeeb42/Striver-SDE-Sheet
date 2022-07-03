09a4f7324a865e3bb2d840f8563ce81d9550a50f
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-07-03 23:39:08 +0530

    Update 1_0_1_Knapsack.cpp

--- DP Series/AV/1_0_1_Knapsack.cpp
+++ DP Series/AV/1_0_1_Knapsack.cpp
@@ -22,7 +22,37 @@ int solve(vector<vector<int>>&dp,vector<int> &values, vector<int> &weights, int 
 int maxProfit(vector<int> &values, vector<int> &weights, int n, int w)
 {
 	// Write your code here
-    vector<vector<int>>dp(n+1,vector<int>(w+1,-1));
-    return solve(dp,values,weights,n,w);
+//     vector<vector<int>>dp(n+1,vector<int>(w+1,-1));
+//     return solve(dp,values,weights,n,w);
+     vector<vector<int>>dp(n+1,vector<int>(w+1,-1));
+    
+    //base case
+    for(int i=0;i<=n;i++){
+        dp[i][0]=0;
+    }   
+    for(int j=0;j<=w;j++){
+        dp[0][j]=0;
+    }
+    
+    //recursive cases
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=w;j++)
+        {
+            if(weights[i-1]<=j)
+            {
+                // Pick or not pic maximum
+               dp[i][j]=max(values[i-1]+dp[i-1][j-weights[i-1]],
+                                 dp[i-1][j]);
+            }
+            
+            else if(weights[i-1]>j)
+            {
+               dp[i][j]=dp[i-1][j];
+            }    
+        }
+    }
+    
+   return dp[n][w];
     
 }
