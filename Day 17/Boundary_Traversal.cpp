9335dcd9619bf3d08df3e4d19e9d7e6b702c6543
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-07-08 17:50:24 +0530

    Update Boundary_Traversal.cpp

--- Day 17/Boundary_Traversal.cpp
+++ Day 17/Boundary_Traversal.cpp
@@ -47,11 +47,12 @@ void rightView(Node* root,vector<int>&ans){
         if(root==NULL) return ans;
         
         ans.push_back(root->data);
+       // if(root->left==NULL && root->right==NULL)return ans;
         
         leftView(root->left,ans);
         bottomview(root->left,ans);
         bottomview(root->right,ans);
-        // bottomview(root,ans);// This will not work since if only one node present in tree, It will
+        //bottomview(root,ans);// This will not work since if only one node present in tree, It will
         //be printed twice
         
         rightView(root->right,ans);
