e1a85fc2a6b1741e8b0edb3d163c1e51e9cf5985
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-07-08 13:07:14 +0530

    Update Morris_Inorder.cpp

--- Day 17/Morris_Inorder.cpp
+++ Day 17/Morris_Inorder.cpp
@@ -1,15 +1,5 @@
-/*
-    Following is Binary Tree Node structure:
-    class TreeNode
-    {
-    public:
-        int data;
-        TreeNode *left, *right;
-        TreeNode() : data(0), left(NULL), right(NULL) {}
-        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
-        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
-    };
-*/
+//chill maro ..itna tough bhi nhi hai
+//refernce -Love babbar lec 68
 vector<int> getInOrderTraversal(TreeNode *root)
 {
     // Write your code here.
