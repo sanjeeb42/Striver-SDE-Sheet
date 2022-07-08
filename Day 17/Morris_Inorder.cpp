26f3427a29d836b76576c0767acd8e17051d451b
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-07-08 13:06:17 +0530

    Create Morris_Inorder.cpp

---
+++ Day 17/Morris_Inorder.cpp
@@ -0,0 +1,57 @@
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+vector<int> getInOrderTraversal(TreeNode *root)
+{
+    // Write your code here.
+    
+     vector<int>ans;
+        
+        TreeNode *curr=root;
+        while(curr!=NULL)
+        {
+            
+            if(curr->left==NULL)
+            {
+                // We can print it as L root R and Left is NULL
+                ans.push_back(curr->data);
+                curr=curr->right;
+            }
+            
+            //If curr ka left exists
+            else{
+                //Find the last Node that will be visited before visited root
+                //The last Node will be rightmost node of the leftsubtree
+                TreeNode *temp=curr->left;
+                while(temp->right!=NULL and temp->right!=curr){
+                    temp=temp->right;
+                }
+                
+                //Create a link of the rightnmost node of left subtree with root
+                if(temp->right==NULL){
+                    temp->right=curr;
+                    curr=curr->left;
+                }
+                
+                //Delete link if it exists and print it as all leftsubtree visisted
+                else if(temp->right==curr)
+                {
+                    temp->right==NULL;
+                    ans.push_back(curr->data);
+                    curr=curr->right;
+                }
+                
+            }
+        }
+        
+        return ans;
+}
