3ee2f86d3b7a6a2fd17a31509812b48d144af3c1
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-07-14 12:43:24 +0530

    Update From_inorder_preorder.cpp

--- Day 19/From_inorder_preorder.cpp
+++ Day 19/From_inorder_preorder.cpp
@@ -1,32 +1,31 @@
-class Solution {
-public:
-    int preindex=0;
-    
-    TreeNode* solve(vector<int>&preorder,vector<int>&inorder,int start,int end){
-        if(start>end)return NULL;
-        
-        TreeNode* root=new TreeNode(preorder[preindex++]);
+ TreeNode<int>* solve(vector<int>&inorder,vector<int>&preorder,int start,int end,int &preindex,unordered_map<int,int>&mp)
+    {
+        if(start>end)return NULL;    
+        TreeNode<int>* root=new TreeNode<int>(preorder[preindex++]);
         
         //Finding Breakindex
-        int breakindex;
-        for(int i=start;i<=end;i++){
-            if(inorder[i]==root->val){
-                breakindex=i;
-                break;
-            }
-        }
+        int breakindex=mp[root->data];
+//         for(int i=start;i<=end;i++){
+//             if(inorder[i]==root->data){
+//                 breakindex=i;
+//                 break;
+//             }
+//         }
+              
+        root->left=solve(inorder,preorder,start,breakindex-1,preindex,mp);
+        root->right=solve(inorder,preorder,breakindex+1,end,preindex,mp);
         
-        
-        root->left=solve(preorder,inorder,start,breakindex-1);
-        root->right=solve(preorder,inorder,breakindex+1,end);
-        
-        return root;
-        
-        
+        return root;       
     }
-    
-    
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return solve(preorder,inorder,0,preorder.size()-1);
+
+TreeNode<int> *buildBinaryTree(vector<int> &inorder, vector<int> &preorder)
+{
+	//    Write your code here
+    int preindex=0;
+    int n=preorder.size();
+    unordered_map<int,int>mp;
+    for(int i=0;i<n;i++){
+        mp[inorder[i]]=i;
     }
-};
+    return solve(inorder,preorder,0,preorder.size()-1,preindex,mp);
+}
