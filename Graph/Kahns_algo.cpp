4e7d8d9b2378b5e070c2222b1827814c8d606069
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-07-22 01:07:57 +0530

    Update Kahns_algo.cpp

--- Graph/Kahns_algo.cpp
+++ Graph/Kahns_algo.cpp
@@ -1,35 +1,47 @@
-#include <queue>
-
-vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e) {
-    // Creating adjacency list.
-    vector<vector<int>> adj(v);
-    for(int i = 0; i < e; i++) {
-        adj[edges[i][0]].push_back(edges[i][1]);
-    } 
-    // Calculaing indegree of each vertex.
-    vector<int> indegree(v);
-    for(auto edge : edges) {
-        indegree[edge[1]]++;
-    }
-    // Push all vertices of indegree 0 in queue.
-    queue<int> que;
-    for(int i = 0; i < v; i++) {
-        if(indegree[i] == 0) {
-            que.push(i);
-        }
-    }
-    vector<int> result; // It will store topological sort of the given graph.
-    // Finding topologial sorting
-    while(!que.empty()) {
-        int src = que.front();
-        que.pop();
-        result.push_back(src);
-        for(int node : adj[src]) {
-            indegree[node]--;
-            if(indegree[node] == 0) {
-                que.push(node);
-            }
-        }
-    }
-    return result;
-}
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int>indegree(V,0);
+	    vector<bool>visited(V,false);
+	    unordered_map<int,vector<int>>mp;
+	    for(int i=0;i<V;i++)
+	    {
+	        for(auto it:adj[i])
+	        {
+	            mp[i].push_back(it);
+	            
+	        }
+	    }
+	   
+	    for(auto it: mp){
+	        for(auto pq:it.second){
+	            indegree[pq]++;
+	        }
+	    }
+	    vector<int>ans;
+	    queue<int>q;
+	    for(int i=0;i<V;i++){
+	        if(indegree[i]==0){
+	            q.push(i);
+	            visited[i]=true;
+	        }
+	    }
+	    while(q.size()>0){
+	        
+	        int front=q.front();
+	        q.pop();
+	        ans.push_back(front);
+	        for(auto it: mp[front])
+	        {
+	            if(!visited[it]){
+	                indegree[it]--;
+	                if(indegree[it]==0){
+	                    q.push(it);
+	                visited[it]=true;
+	                }
+	            }
+	        }
+	    }
+	   
+	    return ans;
+	}
