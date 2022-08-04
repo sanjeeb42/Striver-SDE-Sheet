66ee12713e4364e8f7f5abb970bb51959ebaeac5
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-08-04 23:36:06 +0530

    Create Hashing.cpp

---
+++ Hashing.cpp
@@ -0,0 +1,28 @@
+class Solution{
+  public:
+    vector <int> countDistinct (int arr[], int n, int k)
+    {
+        //code here.
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int i=0;i<k-1;i++){
+            mp[arr[i]]++;
+        }
+        
+        int j=-1;
+        
+        for(int i=k-1;i<n;i++)
+        {
+            mp[arr[i]]++;
+            ans.push_back(mp.size());
+            j++;
+            mp[arr[j]]--;
+            if(mp[arr[j]]==0){
+                mp.erase(arr[j]);
+            }
+        }
+       // ans.push_back(mp.size());
+        
+        return ans;
+    }
+};
