691770dc97064af3f1757553a6ee43aabdaea9e8
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-09-09 23:32:04 +0530

    Create Save_boat.cpp

---
+++ Two Pointer/Save_boat.cpp
@@ -0,0 +1,20 @@
+int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int ans=0;
+        int i=0, j=people.size()-1;
+        
+        while(i<j){
+            if(people[i]+people[j]<=limit)
+            {
+                i++,j--;
+            }
+            else j--;
+            
+            ans++;
+        }
+        if(i==j){
+            ans++;
+        }
+        return ans;
+    }
+    
