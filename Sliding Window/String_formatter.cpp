7b1a7132864fded6b3cf35c6fa8e1daf1c574110
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-09-21 23:54:41 +0530

    Create String_formatter.cpp

---
+++ Sliding Window/String_formatter.cpp
@@ -0,0 +1,33 @@
+string ReFormatString(string s, int K){
+    	string ans="";
+    	int n=s.length();
+        int foundfirst=-1;
+        
+    	for(int j=0;j<n;j++)
+    	{
+    	    if(s[j]=='-'){
+    	        if(foundfirst==-1){
+    	            foundfirst=j;
+    	        }
+    	        continue;
+    	    }
+    	    if(s[j]>='a' && s[j]<='z'){
+    	        ans.push_back('A'+(s[j]-'a'));
+    	    }
+    	    else ans.push_back(s[j]);
+    	        
+    	}
+    	
+    	//Done Traversing the first part
+    	int cnt=0;
+    	for(int j=ans.length()-1;j>=0;j--){
+    	    if(cnt==K){
+    	        ans.insert(ans.begin()+j+1,'-');
+    	        cnt=0;
+    	    }
+    	    cnt++;
+    	    
+    	}
+    	
+    	return ans;
+    }
