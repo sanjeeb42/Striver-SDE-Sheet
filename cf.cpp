a5d981e28a91236a2f7df00865d52a46c66b4c5c
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-11-02 23:15:04 +0530

    Create cf.cpp

---
+++ cf.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
